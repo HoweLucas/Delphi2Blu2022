196918be36a121c76bec5e75b0db6d2f287b7e82
FURB\bcmsilva <brunocesarms@outlook.com>
2022-10-04 20:09:52 -0300

    Alteração -  exercício 35.2

--- Conta de luz/Luz.dfm
+++ Conta de luz/Luz.dfm
@@ -2,8 +2,8 @@ object Form1: TForm1
   Left = 0
   Top = 0
   Caption = 'Form1'
-  ClientHeight = 281
-  ClientWidth = 484
+  ClientHeight = 231
+  ClientWidth = 470
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -14,8 +14,8 @@ object Form1: TForm1
   PixelsPerInch = 96
   TextHeight = 13
   object Label1: TLabel
-    Left = 152
-    Top = 24
+    Left = 160
+    Top = 8
     Width = 143
     Height = 16
     Caption = 'Calcular Conta de Luz '
@@ -27,13 +27,11 @@ object Form1: TForm1
     ParentFont = False
   end
   object Label2: TLabel
-    Left = 32
+    Left = 48
     Top = 46
-    Width = 405
+    Width = 362
     Height = 14
-    Caption = 
-      'Informe se o cliente '#233' de residencia, comercio ou de uma industr' +
-      'ia'
+    Caption = 'Tipo de cliente: Resid'#234'ncia, Com'#233'rcio, Ind'#250'stria ou Fazenda'
     Font.Charset = DEFAULT_CHARSET
     Font.Color = clWindowText
     Font.Height = -12
@@ -41,37 +39,54 @@ object Form1: TForm1
     Font.Style = [fsBold]
     ParentFont = False
   end
+  object Label3: TLabel
+    Left = 104
+    Top = 72
+    Width = 81
+    Height = 13
+    Caption = 'KW/h Consumido'
+  end
+  object Label4: TLabel
+    Left = 104
+    Top = 104
+    Width = 86
+    Height = 13
+    Caption = 'Valor de desconto'
+  end
+  object Label5: TLabel
+    Left = 40
+    Top = 185
+    Width = 42
+    Height = 16
+    Caption = 'Label5'
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -13
+    Font.Name = 'Tahoma'
+    Font.Style = [fsBold]
+    ParentFont = False
+  end
   object Button1: TButton
-    Left = 176
-    Top = 66
+    Left = 192
+    Top = 130
     Width = 89
     Height = 33
     Caption = 'Calcular'
     TabOrder = 0
     OnClick = Button1Click
   end
-  object Tresidencia: TEdit
-    Left = 104
-    Top = 112
-    Width = 265
+  object Edit1: TEdit
+    Left = 248
+    Top = 66
+    Width = 121
     Height = 21
     TabOrder = 1
-    Text = 'resid'#234'ncia'
   end
-  object Tcomercio: TEdit
-    Left = 104
-    Top = 155
-    Width = 265
+  object Edit2: TEdit
+    Left = 248
+    Top = 103
+    Width = 121
     Height = 21
     TabOrder = 2
-    Text = 'Com'#234'rcio'
-  end
-  object Tindustria: TEdit
-    Left = 104
-    Top = 198
-    Width = 265
-    Height = 21
-    TabOrder = 3
-    Text = 'Ind'#250'stria'
   end
 end
--- Conta de luz/Luz.pas
+++ Conta de luz/Luz.pas
@@ -11,9 +11,11 @@ type
     Label1: TLabel;
     Button1: TButton;
     Label2: TLabel;
-    Tresidencia: TEdit;
-    Tcomercio: TEdit;
-    Tindustria: TEdit;
+    Edit1: TEdit;
+    Label3: TLabel;
+    Edit2: TEdit;
+    Label4: TLabel;
+    Label5: TLabel;
     procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
@@ -31,50 +33,51 @@ implementation
 procedure TForm1.Button1Click(Sender: TObject);
 var
   Tpcliente: Integer;
-  resvalor : Double;
-  covalor  : Double;
-  indvalor : Double;
-  consumo  : Double;
+  resvalor, covalor, indvalor, consumo, fazendavalor, valordesconto :Double;
   xresposta: String;
 
 begin
   resvalor := 0;
   covalor  := 0;
   indvalor := 0;
+  fazendavalor := 0;
 
-  repeat
-    Tpcliente := strtoint(inputbox('Informe ', ' Informe o tipo de residência: 1 - Residência, 2 - Comércio, 3 - Indústria ',''));
-
-    consumo   := StrToFloat(inputbox('informe ', 'Informe o consumo de kw/h: ',''));
+    consumo       := StrToFloat(Edit1.Text);
+    valordesconto := StrToFloat(Edit2.Text);
+    Tpcliente := strtoint(inputbox('Tipo de Residência ', 'Informe o tipo de residência: 1 - Residência, 2 - Comércio, 3 - Indústria, 4 - Fazenda ',''));
 
 
     if Tpcliente = 1 then
     begin
-      resvalor := consumo*0.60;
+      resvalor := (consumo*0.60) - valordesconto;
+      Label5.Caption:= ('O valor da conta residencial é R$' +FormatFloat('###,###,###.00',resvalor))
     end;
 
     if Tpcliente = 2 then
 
     begin
-      covalor := consumo*0.48;
-    end;
+      covalor := (consumo*0.48) - valordesconto;
+      Label5.Caption:= ('O valor da conta comercial é R$ ' +FormatFloat('###,###,###.00',covalor))
 
+    end;
 
     if Tpcliente = 3 then
 
     begin
-      indvalor := consumo*1.29;
+      indvalor := (consumo*1.29) - valordesconto;
+      Label5.Caption:= ('O valor da conta industrial é R$ ' +FormatFloat('###,###,###.00',indvalor))
     end;
 
-    xresposta := (inputbox('continuar ', ' deseja continuar S ou N ',''));
+      if Tpcliente = 4 then
 
-  until xresposta ='N';
+    begin
+      fazendavalor := (consumo*2.18) - valordesconto;
+      Label5.Caption:= ('O valor da conta fazendária é R$ ' +FormatFloat('###,###,###.00',fazendavalor))
+    end;
 
-  Tresidencia.text := ('Valor da conta da residência é de: ' +FloatToStr(resvalor));
-  tcomercio.text :=('valor da conta do comércio é de: ' +FloatToStr(covalor));
-  tindustria.text := ('valor da conta da indústria é de: ' +FloatToStr(indvalor));
 
 end;
+
 
 
 
